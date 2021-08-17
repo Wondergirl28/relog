65f005b11ae090080203b1917e37da57ba580346
Wondergirl28 <88722170+Wondergirl28@users.noreply.github.com>
2021-08-16 20:14:59 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    let segundos = 0
+    let minutos = 0
+    let horas = 0
+    if (horas == 12) {
+        basic.showIcon(IconNames.Heart)
+    }
+    if (minutos == 59) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (segundos == 60) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
